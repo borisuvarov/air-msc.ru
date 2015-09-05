ec1deeb5280a2e05ef9c453268d53d9fb3c20cb4
Boris Uvarov <boris.uwarow@gmail.com>
2015-09-05 18:09:41 +0300

    Some changes

--- airmsc_ru/static/js/script.js
+++ airmsc_ru/static/js/script.js
@@ -337,7 +337,7 @@ var form = $("#main_form");
                         url_var = '/change/';
                         $('#select_all').on('click', app.selectStations);
                         $('#unselect_all').on('click', app.unselectStations);
-                        form = $("#change-form");
+                        //form = $("#change-form");
                         form.on('submit', app.submitForm);
                     });
 
