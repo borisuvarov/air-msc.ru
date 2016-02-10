9a3fa44f83cb98c6c92f418a4b8f2ab284c07687
Boris Uvarov <boris.uwarow@gmail.com>
2016-02-11 01:55:24 +0300

    some minor changes just to warm up

--- airmsc_ru/airmsc/views.py
+++
@@ -1,7 +0,0 @@
-# from django.shortcuts import render
-
-
-# def home(request):
-#     template = "home.html"
-#     context = {}
-#     return render(request, template, context)
--- airmsc_ru/run/cronstart.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-/usr/bin/crontab
-cd /home/djangoair/airmsc/air/airmsc_ru
-exec crontab crontab
