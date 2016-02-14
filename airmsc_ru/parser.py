81d1cf1b6a7fdee236e3ed1507f132f29b997332
Boris Uvarov <boris.uwarow@gmail.com>
2016-02-14 23:51:38 +0300

    work in process

--- airmsc_ru/parser.py
+++ airmsc_ru/parser.py
@@ -210,7 +210,7 @@ def get_recipients(overpdk_list_all_stations):
             if not temp_stations:
                 recipients_and_stations[address] = set()
             recipients_and_stations[address].update((station_name_id, station_name))
-    sys.stdout.write(recipients_and_stations)
+    sys.stdout.write(str(recipients_and_stations))
     return recipients_and_stations
 
 
