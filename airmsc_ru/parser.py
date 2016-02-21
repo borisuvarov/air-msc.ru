8845e21c6c214377adc4167696962971cdc616f1
Boris Uvarov <boris.uwarow@gmail.com>
2016-02-21 22:39:55 +0300

    test to check if station is working

--- airmsc_ru/parser.py
+++ airmsc_ru/parser.py
@@ -119,7 +119,7 @@ def get_actual_concentrations(parsed_body):
                 parsed_body.xpath(
                     '/html/body/table/tr[50]/td[1]/b/text()'
                 )[0].split()[0], '%d.%m.%Y')
-            print(day)
+            print(day == datetime.date.today())
         except Exception as e:
             sys.stdout.write(str(e))
             day = None
