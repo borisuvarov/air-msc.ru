77e6609d299eee71a9731fd3ed2d185cc0485cd5
Boris Uvarov <boris.uwarow@gmail.com>
2016-02-23 00:53:33 +0300

    charts, work in progress

--- airmsc_ru/static/js/charts.js
+++ airmsc_ru/static/js/charts.js
@@ -4,51 +4,50 @@ $(function() {
         samplePayload = {'station': 'kapotnya'};
 
 
-    $.get('/charts-data/', samplePayload, function(data) {chartData = data}, 'json');
+    $.get('/charts-data/', samplePayload, function(chartData) {drawChart(chartData)}, 'json');
 
-    console.log(chartData);
+    function drawChart() {
+        console.log(chartData);
+        var chart = AmCharts.makeChart("chartdiv", {
+            "theme": "light",
+            "type": "serial",
+            "dataProvider": chartData,
+            "valueAxes": [{
+                "inside": true,
+                "axisAlpha": 0
+            }],
+            "graphs": [{
+                "id": "g1",
+                "balloonText": "<div style='margin:5px; font-size:19px;'><span style='font-size:13px;'>[[category]]</span><br>[[value]]</div>",
+                "bullet": "round",
+                "bulletBorderAlpha": 1,
+                "bulletBorderColor": "#FFFFFF",
+                "hideBulletsCount": 50,
+                "fillAlphas": 1,
+                "lineThickness": 2,
+                "lineColor": "red",
+                "negativeLineColor": "green",
+                "negativeBase": 12,
+                "valueField": "visits"
+            }],
+            "chartScrollbar": {},
+            "chartCursor": {},
+            "categoryField": "date",
+            "categoryAxis": {
+                "parseDates": true,
+                "axisAlpha": 0,
+                "minHorizontalGap": 55
+            }
+        });
 
-    var chart = AmCharts.makeChart("chartdiv", {
-        "theme": "light",
-        "type": "serial",
-        "dataProvider": chartData,
-        "valueAxes": [{
-            "inside":true,
-            "axisAlpha": 0
-        }],
-        "graphs": [{
-            "id":"g1",
-            "balloonText": "<div style='margin:5px; font-size:19px;'><span style='font-size:13px;'>[[category]]</span><br>[[value]]</div>",
-            "bullet": "round",
-            "bulletBorderAlpha": 1,
-            "bulletBorderColor": "#FFFFFF",
-            "hideBulletsCount": 50,
-            "fillAlphas": 1,
-            "lineThickness": 2,
-            "lineColor": "red",
-            "negativeLineColor": "green",
-            "negativeBase": 12,
-            "valueField": "visits"
-        }],
-        "chartScrollbar": {
+        chart.addListener("dataUpdated", zoomChart);
+        zoomChart();
 
-        },
-        "chartCursor": {},
-        "categoryField": "date",
-        "categoryAxis": {
-            "parseDates": true,
-            "axisAlpha": 0,
-            "minHorizontalGap": 55
-        }
-    });
-
-    chart.addListener("dataUpdated", zoomChart);
-    zoomChart();
-
-    function zoomChart() {
-        if (chart) {
-            if (chart.zoomToIndexes) {
-                chart.zoomToIndexes(130, chartData.length - 1);
+        function zoomChart() {
+            if (chart) {
+                if (chart.zoomToIndexes) {
+                    chart.zoomToIndexes(130, chartData.length - 1);
+                }
             }
         }
     }
