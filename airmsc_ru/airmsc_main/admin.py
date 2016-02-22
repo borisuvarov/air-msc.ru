499e13591f8c04b558d97bb791597dc140d1e398
Boris Uvarov <boris.uwarow@gmail.com>
2016-02-22 14:02:38 +0300

    Memberdata model changes

--- airmsc_ru/airmsc_main/admin.py
+++ airmsc_ru/airmsc_main/admin.py
@@ -3,10 +3,13 @@ from .models import Member
 
 
 class MemberAdmin(admin.ModelAdmin):
-    list_display = ['email', 'stations_count']
+    list_display = ['email', 'stations_count', 'stations_names']
 
     def stations_count(self, obj):
-        return dir(obj.memberdata_set.get())
+        return obj.memberdata_set.get().subscribitions_count
+
+    def stations_names(self, obj):
+        return obj.memberdata_set.get().subscribitions
 
     class Meta:
         model = Member
