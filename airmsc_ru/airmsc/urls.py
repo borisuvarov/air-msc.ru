f96cf950fe0d022cae112d85832160646674484c
Boris Uvarov <boris.uwarow@gmail.com>
2016-02-22 22:58:33 +0300

    charts, work in progress

--- airmsc_ru/airmsc/urls.py
+++ airmsc_ru/airmsc/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     url(r'^login/', 'airmsc_main.views.user_login', name='user_login'),
     url(r'^loginform/', 'airmsc_main.views.login_form', name='login_form'),
     url(r'^change/', 'airmsc_main.views.change', name='change'),
+    url(r'^charts-data/', 'airmsc_main.views.get_data_for_chart', name='charts_data'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^django-rq/', include('django_rq.urls')),
 ]
