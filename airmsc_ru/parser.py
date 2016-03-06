c53e2ba13d905913e72d2c327b9964ad3bc83b2a
Boris Uvarov <boris.uwarow@gmail.com>
2016-03-06 15:53:39 +0300

    debug statements

--- airmsc_ru/parser.py
+++ airmsc_ru/parser.py
@@ -217,8 +217,6 @@ def send_email(overpdk_list_all_stations):
                     r.incr('sent_today')
                 except Exception as e:
                     sys.stdout.write(str(e))
-            sys.stdout.write(station_names)
-            sys.stdout.write(member.memberdata_set.get().poisoned_stations)
             if station_names and station_names != member.memberdata_set.get().poisoned_stations:
                 emailvars = {'stations': station_names, 'poisons': poison_names, 'unsubscribe':
                              'http://air-msc.ru/unsubscribe/' + '?' + 'pochta=' + recipient + '&'
