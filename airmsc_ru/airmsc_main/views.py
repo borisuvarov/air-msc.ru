e9c31badd1393758b8b42b5d023a7260081c64e2
Boris Uvarov <boris.uwarow@gmail.com>
2016-03-07 14:33:19 +0300

    debug statements

--- airmsc_ru/airmsc_main/views.py
+++ airmsc_ru/airmsc_main/views.py
@@ -82,6 +82,7 @@ def get_data_for_chart(request):
         conn.close()
         data_provider = OrderedDict()
         for entry in data:
+            sys.stdout.write(entry)
             date = str(entry[0])
             if date not in data_provider:
                 data_provider[date] = [(entry[1], entry[2])]
