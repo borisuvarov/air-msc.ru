49fd52340c78067b0a8de9f736e74d6dcdf0b00d
Boris Uvarov <boris.uwarow@gmail.com>
2016-03-07 20:42:53 +0300

    debug

--- airmsc_ru/static/js/charts.js
+++ airmsc_ru/static/js/charts.js
@@ -34,7 +34,7 @@ $(function() {
         console.log(chartData);
         var chartdiv = '"kapotnya-' + poison + '"';
         console.log(chartdiv);
-        var chart = AmCharts.makeChart('kapotnya-CO', {
+        var chart = AmCharts.makeChart(chartdiv, {
             "theme": "light",
             "type": "serial",
             "dataProvider": chartData,
