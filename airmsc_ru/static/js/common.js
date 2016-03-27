23b66182969898e7b898e0c8d4c6515a07814972
Boris Uvarov <boris.uwarow@gmail.com>
2016-03-27 21:52:36 +0300

    js optimization

--- airmsc_ru/static/js/common.js
+++ airmsc_ru/static/js/common.js
@@ -8,7 +8,8 @@ var myMap,
 function init() {
     myMap = new ymaps.Map("map", {
         center: [55.76, 37.64],
-        zoom: 11
+        zoom: 11,
+        controls: ["zoomControl"]
     });
 
     myMap.controls.add('zoomControl');
